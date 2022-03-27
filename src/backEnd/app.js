0162fd16691c2d25b6e500f0ca5f4b36153d41fd
Zhe Chen <zhc565@usask.ca>
2022-03-27 14:34:03 -0600

    install bycrypt

--- src/backEnd/app.js
+++ src/backEnd/app.js
@@ -21,7 +21,7 @@ app.use(express.static('../frontEnd/the_actually_website'));
 var mysqlConnection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Assassin990118',
+    password: 'rootroot',
     database: 'cmpt370'
 });
 
